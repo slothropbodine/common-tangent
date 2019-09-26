fca2044d5ff40295684a9ed5ff724713075bbd91
slothropbodine <slothropbodine@gmail.com>
2019-09-26 11:09:21 +0000

    Don't instantiate for static functions.

--- src/main/java/com/mathfun/commontangent/App.java
+++ src/main/java/com/mathfun/commontangent/App.java
@@ -11,7 +11,7 @@ public class App
         //System.out.println( "Hello World!" );
         Circle c1 = new Circle(new Point(0, 0), 5);
         Circle c2 = new Circle(new Point(10, 30), 1);
-        Point ext = new Solver().findExtDivPoint(c1, c2);
+        Point ext = Solver.findExtDivPoint(c1, c2);
         System.out.printf( "%f, %f\n", ext.getX(), ext.getY() );
     }
 }
